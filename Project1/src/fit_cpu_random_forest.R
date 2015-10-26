937eaeb1d08834f41622aac70ffd30f95e1d4719
Thomas Denoréaz <thomas.denoreaz@gmail.com>
2015-10-26 22:13:07 +0100

    [Ex01] R - Random Forest -> Included single term

--- Project1/src/fit_cpu_random_forest.R
+++ Project1/src/fit_cpu_random_forest.R
@@ -5,11 +5,11 @@ valid_input <- read.csv('../data/validate_and_test_r.csv')
 # (Width + ROB + IQ + LSQ + RFs + RF.read + RF.write + Gshare + BTB + Branches + L1.I + L1.D + L2.U + Depth)^2
 
 set.seed(131)
-rf <- randomForest(Delay ~ (Width + ROB + IQ + LSQ + RFs + RF.read + RF.write + Gshare + BTB + Branches + L1.I + L1.D + L2.U + Depth)^2,
+rf <- randomForest(Delay ~ (1 + Width + ROB + IQ + LSQ + RFs + RF.read + RF.write + Gshare + BTB + Branches + L1.I + L1.D + L2.U + Depth)^2,
                    data = train_input,
                    mtry=14,
                    ntree=500)
 print(rf)
 
 predicted <- (predict(rf, valid_input))
-write.csv(predicted, 'r_predicted.csv')
+write.csv(predicted, 'r_predicted3.csv')
