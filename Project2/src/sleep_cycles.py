1ac127a827ff3407eaf4333a92e9908c2b920f79
tyrann <thomas.j.rubeli@gmail.com>
2015-11-09 22:38:04 +0100

    modified classifier

--- Project2/src/sleep_cycles.py
+++ Project2/src/sleep_cycles.py
@@ -7,6 +7,10 @@ import sklearn
 #SVM functions
 from sklearn import svm
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.feature_selection import VarianceThreshold
+
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
 
 if __name__ == '__main__':
 
@@ -14,8 +18,17 @@ if __name__ == '__main__':
     train_set = set[:,1:7]
     train_labels = set[:,8]
 
+    X, y = train_set, train_labels
+    print train_set.size
+
+
+    # Preprocessing
+
+    # train_set = SelectKBest(chi2, k=2).fit_transform(X, y)
+
+
     clf = svm.SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
-    decision_function_shape=None, degree=3, gamma='auto', kernel='linear',
+    decision_function_shape=None, degree=2, gamma='auto', kernel='rbf',
     max_iter=-1, probability=False, random_state=None, shrinking=True,
     tol=0.001, verbose=False)
 
